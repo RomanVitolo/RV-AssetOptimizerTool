2c27a1998e8657d4a652f35cff62d9fbd7260d2f
RomanVitolo <loktheriao@gmail.com>
2025-03-14 23:56:54 -0300

    Initial Package Version

---
+++ RV - AssetOptimizer Package/Assets/RV - AssetOptimizerSystem/Editor/ModelOptimizationProfile.cs
@@ -0,0 +1,9 @@
+namespace RV_AssetOptimizerSystem.Editor
+{
+    [System.Serializable]
+    public class ModelOptimizationProfile 
+    {
+        public string profileName;
+        public float modelPolygonReduction;
+    }
+}
